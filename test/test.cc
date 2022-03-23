d160fbc9ab143591e8dcda0e6704c32c38c28b04
Jo Zou <jo_zou@wistron.com>
2022-03-23 17:14:03 +0800

    cpp

--- test/test.cc
+++ test/test.cc
@@ -1,16 +1,15 @@
-#include <stdio.h>
 #include <iostream>
+#include <math.h>
+#include <stdio.h>
 
 using namespace std;
-void test(int &x)
-{
-    x = 12;
-}
-int main(int argc, char const *argv[])
-{
-    int x = 1;
-    test(x);
-    printf("之后的x: %d", x);
-    cout << 1;
-    return 0;
+void test(int &x) { x = 12; }
+int main(int argc, char const *argv[]) {
+  int x = 1;
+  test(x);
+  printf("之后的x: %d\n", x);
+  cout <<(long) pow(2, 24);
+  ;
+  cout << 1;
+  return 0;
 }
